0f4a5d2fad934193a213a40b3d4530ac86eea1f7
BerryMcockiner <96995050+BerryMcockiner@users.noreply.github.com>
2022-01-02 15:52:47 -0500

    Update Rollthedicegame .py

--- Rollthedicegame .py
+++ Rollthedicegame .py
@@ -1,30 +1,28 @@
 import random
+def score:
+    User_Score = 0
+    Comp_Score = 0
 while True:
     game = input("would you like to roll the dice? Y/N")
     if game == "Y":
         User = random.randint(1,6)
-        User_score = 0
         print ("you rolled a", User, "!")
         Comp = random.randint(1,6)
         print ("The computer rolled a", Comp, "!")
         if User > Comp:
             print ("you win!")
+            User_Score+=1
         elif User < Comp:
-                print ("you lose")
+            print ("you lose")
+            Comp_Score+=1
         if User == Comp:
-                print ("its a draw")
-
-        Comp_score = 0
-        User_score = 0
-        if User > Comp:
-            User_score+=1
-            print ("You", User_score)
-        elif User < Comp:
-            Comp_score+=1
-            print ("Comp", Comp_score)
+            print ("its a draw")
+            print(score)
     if game == "N":
         print ("Thank you for playing. Have a great day!")
         break
+    else:
+        print ("Sorry, please say 'Y' or 'N'")
          
 
 
