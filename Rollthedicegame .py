5f581559f261239cb33a34f513156022c8aa2c01
jsmith88ice <46384020+jsmith88ice@users.noreply.github.com>
2022-01-02 15:25:23 -0500

    Revert "RTDUPDATE-1 added player and comp score tracking"

--- Rollthedicegame .py
+++ Rollthedicegame .py
@@ -1,6 +1,4 @@
 import random
-User_Score = 0
-Comp_Score = 0
 while True:
     game = input("would you like to roll the dice? Y/N")
     if game == "Y":
@@ -10,16 +8,10 @@ while True:
         print ("The computer rolled a", Comp, "!")
         if User > Comp:
             print ("you win!")
-            User_Score+=1
-            print (User_Score)
         elif User < Comp:
-            print ("you lose")
-            Comp_Score+=1
-            print (Comp_Score)
+                print ("you lose")
         if User == Comp:
             print ("its a draw")
-        print("Player Score:", User_Score)
-        print("Oppenent Score:", Comp_Score)
     if game == "N":
         print ("Thank you for playing. Have a great day!")
         break
